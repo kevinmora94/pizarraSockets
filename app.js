ddd8e6ddf8af8a3efde6be89e0db3a056c04fac3
Kevin Mora <kevin.morag94@gmail.com>
2016-07-05 09:31:58 -0600

    Procfile jjj

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
 	console.log('pizarra corriendo en port 3000');
 });
